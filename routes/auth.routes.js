235ca70638177c7fb5e75080ff0b365ecbed3c38
Polina <polina.liutsko@gmail.com>
2020-05-16 17:29:04 +0300

    routes registration

---
+++ routes/auth.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const User = require("../models/User");
+const router = Router();
+
+// /api/auth/register
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+  } catch (e) {
+    res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
+  }
+});
+
+// /api/auth/login
+router.post("/login", async (req, res) => {});
+
+module.exports = router;
